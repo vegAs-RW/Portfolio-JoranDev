1dfe78897c8d04fc0b9f1ebc565befaa5532aa24
vegAs-RW <jordan.pouchot2@gmail.com>
2022-05-17 15:51:00 +0200

    dev du style switcher

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing Anim */
+var typed = new Typed(".typing",{
+    strings:["Développeur web","Développeur Back-End","Développeur Full Stack"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
